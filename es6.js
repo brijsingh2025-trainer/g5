c602158222e6abf6106702292731f2195d1a26ba
Mr Singh <sbm43@gmail.com>
2026-09-23 15:07:56 +0530

    #Start to discuss es6 features

---
+++ es6.js
@@ -0,0 +1,83 @@
+// ES6 Features 
+/***
+ *   New addon two variable 1- let , 2 - const before javascript have only one variable that is var 
+ * 
+ * ***/
+
+ // let fname ; //=====  variable Declearation 
+
+ // fname = "Brij ";  // varaiable initialization
+
+ // console.log('fname', fname);
+
+  // For const 
+
+  // it also have a block scope and decleration and initialization both will occured same time 
+
+  // const age = 33;
+
+  //age = 40 // it through error because we can not allow to re-initialization for constant
+
+  //console.log(age);
+
+
+const data = [ 2,3,4,1];
+data.push(77);
+console.log(data);
+
+
+
+
+
+// String Literal 
+let fname = "Mohan";
+
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+/***
+ * 
+ * 
+  console.warn('fname', fname);
+
+  console.error('fname', fname);
+
+  console.dir(fname);
+
+  let user = {
+    name: "Brij Mohan Singh", 
+    age : 40, 
+    department: "IT",
+  }
+
+ // console.table('fname', user);
+
+
+**/
+
+
+
+/*
+ *  
+ *  String literal 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * **/
